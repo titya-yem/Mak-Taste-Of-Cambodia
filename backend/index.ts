0eeef269368ac19fbfeeaff3b2fefac9f2fe2379
Titya Yem <thitya.yem.photo@gmail.com>
2026-04-23 14:14:05 -0400

    Backend: Finished files and folders setup

---
+++ backend/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+
+app.get("/", (req, res) => {
+    res.send("Server is running 🚀");
+});
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
