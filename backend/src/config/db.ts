affdd50b919b48613e07ab70ceb6f7dd83144d7b
Titya Yem <thitya.yem.photo@gmail.com>
2026-04-25 23:58:39 -0400

    Backend: Setup Database

---
+++ backend/src/config/db.ts
@@ -0,0 +1,17 @@
+import {Pool} from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+declare global {
+    var pgPool: Pool | undefined;
+}
+
+const pool: Pool = globalThis.pgPool ?? new Pool ({
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
+});
+
+globalThis.pgPool = pool;
+
+export default pool;
