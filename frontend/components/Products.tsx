c2faa07578277507a1dd67c3f471f5c637a028bf
Titya Yem <thitya.yem.photo@gmail.com>
2026-04-28 23:22:57 -0400

    UI: fixed ambassador page

--- frontend/components/Products.tsx
+++ frontend/components/Products.tsx
@@ -1,49 +1,51 @@
-import fetchApi from "@/hooks/useFetch";
-import { ProductTypes } from "@/types/productTypes";
-import { Flex, Text } from "@radix-ui/themes";
-import Image from "next/image";
-import { Button } from "./ui/button";
+  import fetchApi from "@/hooks/useFetch";
+  import { ProductTypes } from "@/types/productTypes";
+  import { Flex, Text } from "@radix-ui/themes";
+  import Image from "next/image";
+  import { Button } from "./ui/button";
 
-const Products = async () => {
-  const response = await fetchApi<ProductTypes[]>("/product");
-  const products = response.data;
+  const Products = async () => {
+    const result = await fetchApi<ProductTypes[]>("/product");
+    const data = result?.data ?? [];
 
-  return (
-    <div className="flex flex-wrap items-center justify-center md:justify-between gap-6">
-      {products.map((product) => (
-        <div key={product.id} className="w-80">
-          <Image
-            src={product.image}
-            alt={product.name}
-            width={320}
-            height={320}
-            className="rounded-md"
-          />
+    return (
+      <div className="flex flex-wrap items-center justify-center md:justify-between gap-6">
+        {data.map((product) => (
+          <div key={product.id} className="w-70 md:w-75">
+            <Image
+              src={product.image}
+              alt={product.name}
+              width={290}
+              height={290}
+              className="mx-auto rounded-md"
+            />
 
-          <Flex align="center" justify="between">
-            <h5 className="py-2 text-lg md:text-xl font-bold">
-              {product.name}
-            </h5>
+            <Flex align="center" justify="between">
+              <h5 className="py-2 text-lg font-bold">
+                {product.name}
+              </h5>
 
-            <Text className="text-sm md:text-base text-[#702E1C]">
-              ${product.price}
+              <Text className="text-sm font-bold text-[#702E1C]">
+                ${product.price}
+              </Text>
+            </Flex>
+
+            <Text as="p" className="text-sm pb-4 text-[#54433F]">
+              {product.description.length > 120
+                ? product.description.slice(0, 120) + "..."
+                : product.description}
             </Text>
-          </Flex>
 
-          <Text as="p" className="pb-4 text-[#54433F]">
-            {product.description}
-          </Text>
+            <Button
+              variant="ghost"
+              className="w-full py-6 rounded-sm uppercase cursor-pointer text-white bg-[#702E1C] hover:text-white hover:bg-[#7c3825]"
+            >
+              Add To Cart
+            </Button>
+          </div>
+        ))}
+      </div>
+    );
+  };
 
-          <Button
-            variant="ghost"
-            className="w-full py-6 rounded-sm uppercase cursor-pointer text-white bg-[#702E1C] hover:bg-[#7c3825]"
-          >
-            Add To Cart
-          </Button>
-        </div>
-      ))}
-    </div>
-  );
-};
-
-export default Products;
+  export default Products;
