8a89428396cc10c75610fe8330d0066acd06007a
Titya Yem <thitya.yem.photo@gmail.com>
2026-05-12 15:24:56 -0400

    UI: create Sidebar component and routes

--- frontend/app/dashboard/layout.tsx
+++ frontend/app/dashboard/layout.tsx
@@ -1,11 +1,11 @@
-import { Sidebar, SidebarProvider } from "@/components/ui/sidebar";
-import { ReactNode } from "react";
+import { SidebarProvider } from "@/components/ui/sidebar";
+import AppSidebar from "@/components/AppSidebar";
 
-const layout = ({ children }: { children: ReactNode }) => {
+const layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <SidebarProvider>
       <main className="flex min-h-screen">
-        <Sidebar />
+        <AppSidebar />
         <div className="flex-1 p-6">{children}</div>
       </main>
     </SidebarProvider>
