4cdc1f63461e2195106957fad1c050f1792c092c
Titya Yem <thitya.yem.photo@gmail.com>
2026-05-16 21:19:58 -0400

    UI: Refactor code and routes

--- frontend/components/Dashboard/Admin/ActiveOrders.tsx
+++ frontend/components/Dashboard/Admin/ActiveOrders.tsx
@@ -1,9 +1,9 @@
-import { Badge, Flex, Table } from "@radix-ui/themes";
+import { Badge, Flex, Table, Text } from "@radix-ui/themes";
 import Link from "next/link";
 
 const ActiveOrders = () => {
   return (
-    <main className="pt-8">
+    <main className="max-w-132 pt-8">
       <Flex justify="between" align="center" className="pb-4">
         <h5 className="text-lg font-bold">Active Order Queue</h5>
         <Link
@@ -14,33 +14,51 @@ const ActiveOrders = () => {
         </Link>
       </Flex>
 
-      <Table.Root>
-        <Table.Header>
-          <Table.Row>
-            <Table.ColumnHeaderCell>Order ID</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell>Customer</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell>Product</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell>Amount</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell>Status</Table.ColumnHeaderCell>
-          </Table.Row>
-        </Table.Header>
+      {/* mobile cards */}
+      <div className="space-y-3 md:hidden">
+        <div className="p-4 rounded-lg bg-[#F7F3ED]">
+          <Text as="p" className="font-bold">
+            # MAK-8901
+          </Text>
+          <Text as="p" className="text-sm text-gray-600">
+            Sovan J.
+          </Text>
 
-        <Table.Body>
-          <Table.Row className="bg-[#F7F3ED]">
-            <Table.RowHeaderCell># MAK-8901</Table.RowHeaderCell>
-            <Table.Cell>Sovan J.</Table.Cell>
-            <Table.Cell>
-              {"Mak Seassoning".length > 8
-                ? "Mak Seassoning".slice(0, 8) + "..."
-                : "Mak Seassoning"}
-            </Table.Cell>
-            <Table.Cell>$44.97</Table.Cell>
-            <Table.Cell>
-              <Badge color="crimson">Pending</Badge>
-            </Table.Cell>
-          </Table.Row>
-        </Table.Body>
-      </Table.Root>
+          <div className="flex justify-between mt-2">
+            <Text as="span" className="text-sm">
+              $44.97
+            </Text>
+            <Badge color="crimson">Pending</Badge>
+          </div>
+        </div>
+      </div>
+
+      {/* tablet + laptop screen */}
+      <div className="hidden md:block w-full overflow-x-auto">
+        <Table.Root>
+          <Table.Header>
+            <Table.Row>
+              <Table.ColumnHeaderCell>Order ID</Table.ColumnHeaderCell>
+              <Table.ColumnHeaderCell>Customer</Table.ColumnHeaderCell>
+              <Table.ColumnHeaderCell>Product</Table.ColumnHeaderCell>
+              <Table.ColumnHeaderCell>Amount</Table.ColumnHeaderCell>
+              <Table.ColumnHeaderCell>Status</Table.ColumnHeaderCell>
+            </Table.Row>
+          </Table.Header>
+
+          <Table.Body>
+            <Table.Row className="bg-[#F7F3ED]">
+              <Table.RowHeaderCell># MAK-8901</Table.RowHeaderCell>
+              <Table.Cell>Sovan J.</Table.Cell>
+              <Table.Cell>Mak Seassoning</Table.Cell>
+              <Table.Cell>$44.97</Table.Cell>
+              <Table.Cell>
+                <Badge color="crimson">Pending</Badge>
+              </Table.Cell>
+            </Table.Row>
+          </Table.Body>
+        </Table.Root>
+      </div>
     </main>
   );
 };
