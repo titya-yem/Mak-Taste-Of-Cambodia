5ee4c0a983158b1caeb977e81b4f1a5235dc8610
Titya Yem <thitya.yem.photo@gmail.com>
2026-05-26 13:14:19 -0400

    UI: Finished Backup admin dashboard

--- frontend/app/dashboard/admin/backup/page.tsx
+++ frontend/app/dashboard/admin/backup/page.tsx
@@ -1,9 +1,29 @@
-
+import Image from "next/image";
+import backupImage from "@/public/dashboard/admin/Backup.svg";
+import { Text } from "@radix-ui/themes";
+import { Button } from "@/components/ui/button";
 
 const page = () => {
   return (
-    <div>page</div>
-  )
-}
+    <main>
+      <div className="w-fit my-4 p-4 rounded-lg bg-white">
+        <Image
+          src={backupImage}
+          alt="Back up icon"
+          width={40}
+          height={40}
+          className="rounded-sm p-2 bg-[#702E1C]"
+        />
+        <h5 className="text-xl font-semibold">Download Backup</h5>
+        <Text as="p" className="text-sm py-2 text-[#54433F]">
+          Initiate full system snapshot to secure cloud storage.
+        </Text>
+        <Button className="p-4 rounded-md cursor-pointer text-white bg-[#702E1C] hover:text-white hover:bg-[#8e3e27]">
+          Download
+        </Button>
+      </div>
+    </main>
+  );
+};
 
-export default page
+export default page;
